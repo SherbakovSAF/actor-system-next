2f509fcc4df8711050ecb3aefabb673d7c79f15f
SherbakovSAF <sherbakovsaf@gmail.com>
2025-01-24 22:48:22 +0300

    Настройка первичных configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,20 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: { xs: "1rem" },
+    },
     extend: {
+      screens: {
+        xs: "320px",
+        sm: "414px",
+        md: "540px",
+        lg: "768px",
+        xl: "1024px",
+        "2xl": "1280px",
+        "3xl": "1536px",
+      },
       colors: {
         background: "var(--bg-color)",
         foreground: "var(--foreground)",
@@ -15,10 +28,16 @@ export default {
           DEFAULT: "var(--surface-color)",
           secondary: "var(--surface-color-secondary)",
         },
-        // bgAccent: "var(--bg-accent-color)",
-        // accent: "var(--accent-color)",
-        // textPrimary: "var(--text-color-primary)",
-        // textSecondary: "var(--text-color-secondary)",
+        accent: {
+          DEFAULT: "var(--accent-color)",
+        },
+        text: {
+          DEFAULT: "var(--text-color-primary)",
+          secondary: "var(--text-color-secondary)",
+          disabled: "var(--text-color-disabled)",
+        },
+        error: "var(--error-color)",
+        // TODO: Добавить error, success
       },
     },
   },
