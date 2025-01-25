2921409e2f7c2f8a1a331e55996515d0b3853d95
SherbakovSAF <sherbakovsaf@gmail.com>
2025-01-25 12:08:14 +0300

    Добавил раздел с настройками

---
+++ src/app/(default)/settings/page.tsx
@@ -0,0 +1,14 @@
+const SettingsPage = async () => {
+  //  TODO: Получаем user
+  return (
+    <main>
+      <h1>Раздел в разработке.</h1>
+      <p>
+        Чуть позже Вы сможете подностью настроить свой аккаунт и изменить свои
+        настройки
+      </p>
+    </main>
+  );
+};
+
+export default SettingsPage;
--- src/types/route-path.type.ts
+++ src/types/route-path.type.ts
@@ -1,5 +1,6 @@
 export enum RoutePath_E {
   HOME = "/",
+  SETTINGS = "/settings",
   ADMIN = "/admin",
   ADMIN_CINEMATIC = "/admin/cinematic",
   ADMIN_USERS = "/admin/users",
